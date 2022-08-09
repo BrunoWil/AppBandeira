2b87f669d6de3dc4967238e39654aedc7ba7f5e4
Grupo2k22 <bruno.wilson.m@gmail.com>
2022-08-08 20:51:50 -0400

    Updade para o git

---
+++ bandeira/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  bandeira
+//
+//  Created by user220849 on 8/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the v
+
+    }
+    
+}
+
+
+
